b99f3bfcdf6441a717cdc5adb872b7d0c354cd4f
Lanaanvar <118440051+Lanaanvar@users.noreply.github.com>
2023-06-02 08:56:47 +0530

    Add files via upload

---
+++ MAP API/zones.js
@@ -0,0 +1,162 @@
+var dangerZones = [
+  {
+    name: "Angamaly Municipality",
+    coordinates: [
+      [10.2048, 76.3823],
+      [10.2048, 76.3811],
+      [10.2027, 76.381],
+      [10.2026, 76.3818],
+      // Add more coordinate pairs as needed
+    ],
+  },
+  {
+    name: "Chengamanad",
+    coordinates: [
+      [10.2073, 76.3907],
+      [10.2073, 76.3898],
+      [10.2047, 76.3898],
+      [10.2047, 76.3907],
+      // Add more coordinate pairs as needed
+    ],
+  },
+  {
+    name: "Choornikkara",
+    coordinates: [
+      [10.1374, 76.4082],
+      [10.1374, 76.4066],
+      [10.136, 76.4066],
+      [10.136, 76.4082],
+      // Add more coordinate pairs as needed
+    ],
+  },
+  {
+    name: "Edathala",
+    coordinates: [
+      [10.1318, 76.3897],
+      [10.1318, 76.3893],
+      [10.1305, 76.3893],
+      [10.1305, 76.3897],
+      // Add more coordinate pairs as needed
+    ],
+  },
+  {
+    name: "Keezhmad",
+    coordinates: [
+      [10.1353, 76.3131],
+      [10.1353, 76.3116],
+      [10.1327, 76.3116],
+      [10.1327, 76.3131],
+      // Add more coordinate pairs as needed
+    ],
+  },
+  {
+    name: "Parakkadavu",
+    coordinates: [
+      [10.1106, 76.3512],
+      [10.1106, 76.3503],
+      [10.1091, 76.3503],
+      [10.1091, 76.3512],
+      // Add more coordinate pairs as needed
+    ],
+  },
+  {
+    name: "Tripunithura Municipality",
+    coordinates: [
+      [9.9633, 76.3544],
+      [9.9633, 76.3534],
+      [9.9616, 76.3534],
+      [9.9616, 76.3544],
+      // Add more coordinate pairs as needed
+    ],
+  },
+  {
+    name: "Alangad",
+    coordinates: [
+      [10.1547, 76.3939],
+      [10.1547, 76.3946],
+      [10.154, 76.3946],
+      [10.154, 76.3939],
+    ],
+  },
+  {
+    name: "Chendamangalam",
+    coordinates: [
+      [10.1567, 76.2829],
+      [10.1567, 76.2836],
+      [10.156, 76.2836],
+      [10.156, 76.2829],
+    ],
+  },
+  {
+    name: "Chittattukara",
+    coordinates: [
+      [10.3315, 76.3035],
+      [10.3315, 76.3042],
+      [10.3308, 76.3042],
+      [10.3308, 76.3035],
+    ],
+  },
+  {
+    name: "Eloor Municipality",
+    coordinates: [
+      [10.0618, 76.2932],
+      [10.0618, 76.2939],
+      [10.0611, 76.2939],
+      [10.0611, 76.2932],
+    ],
+  },
+  {
+    name: "Ezhikkara",
+    coordinates: [
+      [10.0735, 76.2926],
+      [10.0735, 76.2933],
+      [10.0728, 76.2933],
+      [10.0728, 76.2926],
+    ],
+  },
+  {
+    name: "Kadungalloor",
+    coordinates: [
+      [10.1842, 76.3217],
+      [10.1842, 76.3224],
+      [10.1835, 76.3224],
+      [10.1835, 76.3217],
+    ],
+  },
+  {
+    name: "Kalamassery Municipality",
+    coordinates: [
+      [10.0605, 76.3173],
+      [10.0605, 76.318],
+      [10.0598, 76.318],
+      [10.0598, 76.3173],
+    ],
+  },
+  {
+    name: "Karumallur",
+    coordinates: [
+      [10.1979, 76.4074],
+      [10.1979, 76.4081],
+      [10.1972, 76.4081],
+      [10.1972, 76.4074],
+    ],
+  },
+  {
+    name: "Kottuvally",
+    coordinates: [
+      [10.1927, 76.3522],
+      [10.1927, 76.3529],
+      [10.192, 76.3529],
+      [10.192, 76.3522],
+    ],
+  },
+  {
+    name: "Kunnukara",
+    coordinates: [
+      [10.1433, 76.3134],
+      [10.1433, 76.3141],
+      [10.1426, 76.3141],
+      [10.1426, 76.3134],
+    ],
+  },
+];
